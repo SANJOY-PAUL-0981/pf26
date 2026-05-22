0cdb78919fdc74d2c97c1731b9695f884709177a
SANJOY <paulsanjoy2923@gmail.com>
2026-05-22 09:32:40 +0530

    trying to optimise seo more

---
+++ pf26/app/robots.ts
@@ -0,0 +1,6 @@
+export default function robots() {
+  return {
+    rules: { userAgent: '*', allow: '/' },
+    sitemap: 'https://sanjoydev.com/sitemap.xml',
+  }
+}
---
+++ pf26/app/sitemap.ts
@@ -0,0 +1,22 @@
+export default function sitemap() {
+    return [
+        {
+            url: 'https://sanjoydev.com',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: 'https://sanjoydev.com/projects',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://sanjoydev.com/contact',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.5,
+        },
+    ]
+}
